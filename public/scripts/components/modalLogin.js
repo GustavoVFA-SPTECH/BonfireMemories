8d727a6f7b44be11ed6a86939ffcb8ad3b603431
GustavoVFA-SPTECH <gustavo.famorim@sptech.school>
2024-10-31 20:47:47 -0300

    Component login

---
+++ public/scripts/components/modalLogin.js
@@ -0,0 +1,27 @@
+(function navBar() {
+    const scriptParent = document.currentScript.parentElement;
+    const page = document.currentScript.getAttribute("data-page");
+
+
+    scriptParent.insertAdjacentHTML("beforeend", `
+    <div class="modalLogin">
+      <div class="mainLogin">
+        <div class="loginContent">
+          <div class="loginLogo"></div>
+          <span class="loginLogoText">Bonfire Memories</span>
+        </div>
+        <div class="loginContent">
+          <span class="loginText">Login</span>
+          <div class="inputsLogin">
+            <input class="inputLogin" type="text" placeholder="Username/Email"/>
+            <input class="inputLogin" type="text" placeholder="Password" />
+          </div>
+          <div class="buttonsLogin">
+            <button class="buttonLogin">Back</button>
+            <button class="buttonLogin">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+       `);
+})();
