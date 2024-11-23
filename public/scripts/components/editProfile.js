edb0574833dcd2a5eb02cf5ed8d4f0147fc8abc0
GustavoVFA-SPTECH <gustavo.famorim@sptech.school>
2024-11-22 23:10:43 -0300

    preview foto de perfil

--- public/scripts/components/editProfile.js
+++ public/scripts/components/editProfile.js
@@ -6,7 +6,7 @@ function editModal() {
       `
       <div class="editModal">
         <div class="editMain">
-        <span class="editTitle">Edit Profile</span>
+            <span class="editTitle">Edit Profile</span>
             <div class="previewProfilePicture"></div>
                 <label for="iptProfilePicture" class="labelPicture"></label>
                 <input type="file" id="iptProfilePicture">
@@ -33,3 +33,28 @@ function openEdit(){
     const editModal = document.querySelector(".editModal");
     editModal.style.display = 'flex';
 }
+
+document.getElementById('iptProfilePicture').addEventListener('change', function(event) {
+    const fileInput = event.target;
+    const previewDiv = document.querySelector('.previewProfilePicture');
+
+    previewDiv.style.backgroundImage = '';
+
+    if (fileInput.files && fileInput.files[0]) {
+        const file = fileInput.files[0];
+
+        if (file.type.startsWith('image/')) {
+            const reader = new FileReader();
+
+            reader.onload = function(e) {
+                previewDiv.style.backgroundImage = `url('${e.target.result}')`;
+                previewDiv.style.backgroundSize = '100% 100%';
+                previewDiv.style.backgroundPosition = 'center';
+            };
+
+            reader.readAsDataURL(file);
+        } else {
+            previewDiv.textContent = 'Por favor, selecione um arquivo de imagem.';
+        }
+    }
+});
